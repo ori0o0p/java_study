afdff2a1a966e0ffe85d114f7e62bb88496b48f6
101 <tmddnjs001133@dsm.hs.kr>
2024-03-06 12:00:29 +0900

    thenComparing 추가

--- src/comparator/ComparatorStudy.java
+++ src/comparator/ComparatorStudy.java
@@ -12,10 +12,12 @@ public class ComparatorStudy {
         kims.add(new Kim("김승원", 18, 55));
         kims.add(new Kim("김숭원", 20, 60));
         kims.add(new Kim("원승김", 18, 100));
+        kims.add(new Kim("바보", 18, 55));
 
         kims.sort(Comparator.comparing(Kim::age) // 나이순
                 .reversed() // 역정렬
-                .thenComparing(Kim::weight)); // 나이가 같으면 무게로 정렬
+                .thenComparing(Kim::weight) // 나이가 같으면 무게로 정렬
+                .thenComparing(Kim::name)); // 무게도 같으면 이름으로 정렬
 
         kims.forEach(System.out::println);
     }
