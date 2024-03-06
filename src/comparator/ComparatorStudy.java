e6624948f6afaa7bd27dabfceef283ddc6761fe8
101 <tmddnjs001133@dsm.hs.kr>
2024-03-06 11:58:54 +0900

    Comparator

---
+++ src/comparator/ComparatorStudy.java
@@ -0,0 +1,25 @@
+package comparator;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ComparatorStudy {
+
+    public static void main(String[] args) {
+        List<Kim> kims = new ArrayList<>();
+
+        kims.add(new Kim("김승원", 18, 55));
+        kims.add(new Kim("김숭원", 20, 60));
+        kims.add(new Kim("원승김", 18, 100));
+
+        kims.sort(Comparator.comparing(Kim::age) // 나이순
+                .reversed() // 역정렬
+                .thenComparing(Kim::weight)); // 나이가 같으면 무게로 정렬
+
+        kims.forEach(System.out::println);
+    }
+
+}
+
+record Kim(String name, Integer age, Integer weight) {}
