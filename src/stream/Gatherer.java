4662b64220ab1b392ff257cf3a204dc35ac0fee9
101 <tmddnjs001133@dsm.hs.kr>
2024-03-25 12:33:55 +0900

    stream gatherer 예제 추가

--- src/stream/Gatherer.java
+++ src/stream/Gatherer.java
@@ -8,11 +8,47 @@ public class Gatherer {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
 
+        scan(numbers);
+    }
+
+    public static void fixed(List<Integer> numbers) {
         List<List<Integer>> windows = numbers.stream()
                 .gather(Gatherers.windowFixed(2))
                 .toList();
 
         System.out.println(windows);
+    }
+
+    public static void sliding(List<Integer> numbers) {
+        List<List<Integer>> windows = numbers.stream()
+                .gather(Gatherers.windowSliding(2))
+                .toList();
+
+        System.out.println(windows);
+    }
+
+    public static void fold(List<Integer> numbers) {
+        List<String> list = numbers.stream()
+                .gather(Gatherers.fold(() -> "", (string, number) -> string + number))
+                .toList();
+
+        System.out.println(list);
+    }
+
+    public static void mapConcurrent(List<Integer> numbers) {
+        List<Integer> list = numbers.stream()
+                .gather(Gatherers.mapConcurrent(1, number -> number * number))
+                .toList();
+
+        System.out.println(list);
+    }
+
+    public static void scan(List<Integer> numbers) {
+        List<Integer> list = numbers.stream()
+                .gather(Gatherers.scan(() -> 4, Integer::sum))
+                .toList();
+
+        System.out.println(list);
     }
 
 }
