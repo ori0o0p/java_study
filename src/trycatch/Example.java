4797fd00c16fa770662a5be0d59ea6575a20d8a0
101 <tmddnjs001133@dsm.hs.kr>
2024-04-17 11:47:32 +0900

    비동기 블록 예제 추가

--- src/trycatch/Example.java
+++ src/trycatch/Example.java
@@ -1,9 +1,12 @@
 package trycatch;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 public class Example {
 
     public static void main(String[] args) {
-        example2();
+        example4();
     }
 
     public static void example1() {
@@ -24,7 +27,6 @@ public class Example {
             System.out.println("try 블록 실행");
         }
         catch(ArithmeticException e) {
-            System.out.println("ArithmeticException: catch 블록 실행");
         }
         finally {
             System.out.println("finally 블록 실행");
@@ -33,5 +35,32 @@ public class Example {
         System.out.println("끝"); // 수행 안됌
     }
 
+    public static void example3() {
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> System.out.println(45 / 0));
+
+        try {
+            future.get();
+        }
+        catch(ArithmeticException | InterruptedException | ExecutionException e) {
+            System.out.println("ArithmeticException: catch 블록 실행");
+        }
+    }
+
+    public static void example4() {
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> System.out.println(45 / 0))
+                .exceptionally(e -> {
+                    if (e.getCause() instanceof ArithmeticException) {
+                        System.out.println("ArithmeticException: catch 블록 실행");
+                    }
+                    return null;
+                });
+
+        try {
+            future.get();
+        }
+        catch (ExecutionException | InterruptedException e) {
+            System.out.println("ExecutionException | InterruptedException 실행");
+        }
+    }
 
 }
