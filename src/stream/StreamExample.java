b9d8a40b41c60c9cf85afc57780f53346a581766
101 <tmddnjs001133@dsm.hs.kr>
2024-04-21 21:07:07 +0900

    스트림 예제 추가

--- src/stream/StreamExample.java
+++ src/stream/StreamExample.java
@@ -4,41 +4,65 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class StreamExample {
 
-    public static void main(String[] args) {
-        List<Person> person = List.of(
-                new Person("김승원", 18, true),
-                new Person("바보", 20, true),
-                new Person("멍청이", 13, false),
-                new Person("대마고", 21, false),
-                new Person("이상함", 12, true),
-                new Person("나쁜", 10, false),
-                new Person("똥", 50, true),
-                new Person("승원김", 48, false)
-        );
+    List<Person> person = List.of(
+            new Person("김승원", 18, true),
+            new Person("바보", 20, true),
+            new Person("멍청이", 13, false),
+            new Person("대마고", 21, false),
+            new Person("이상함", 12, true),
+            new Person("나쁜", 10, false),
+            new Person("똥", 50, true),
+            new Person("승원김", 48, false)
+    );
 
-        List<Integer> ages = person.stream()
+    public static void main(String[] args) {
+    }
+
+    public Map<Boolean, Optional<Person>> getPersonMapGroupByStupid() {
+        return person.stream()
+                .collect(Collectors.groupingBy(
+                        Person::isStupid,
+                        Collectors.maxBy(
+                                Comparator.comparingInt(Person::age)
+                        ))
+                );
+    }
+
+    public List<Integer> getFilteredPersonAndSortedByAge() {
+        return person.stream()
                 .filter(p -> p.age() >= 20)
                 .sorted(Comparator.comparingInt(Person::age))
                 .map(Person::age)
                 .toList();
+    }
 
-        List<Map<String, Boolean>> result = person.stream()
+    public List<Map<String, Boolean>> getPersonMap() {
+        return person.stream()
                 .map(p -> Map.of(p.name(), p.isStupid()))
                 .toList();
+    }
 
-        List<Person> seungWonList = person.stream()
+    public List<Person> getPersonList() {
+        return person.stream()
                 .filter(p -> p.name().contains("승원"))
                 .toList();
-
-        Optional<Person> maxAge = person.stream()
-                .max(Comparator.comparingInt(Person::age));
-        
-
     }
+
+    public Optional<Person> getMaxAgePerson() {
+        return person.stream()
+                .max(Comparator.comparingInt(Person::age));
+    }
+
+    public static void print(List<?> list) {
+        list.forEach(System.out::println);
+    }
+
+    public record Person(String name, int age, boolean isStupid) {}
+
 
 }
 
-record Person(String name, int age, boolean isStupid) {}
